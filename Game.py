286a3d67e4a7bb3b5ab4dc7e2bde144427ad4675
AlekseevaMS <Alekseeva.MS@phystech.edu>
2019-12-19 01:14:17 +0300

    подредаченно

--- Game.py
+++ Game.py
@@ -6,14 +6,14 @@ from Brick import *
 import time
 tk = Tk()
 tk.title('Game')
-canvas = Canvas(tk, width=500, height=400, highlightthickness=0)
+canvas = Canvas(tk, width=500, height=400, highlightthickness=0, bg ='black')
 canvas.pack()
 tk.update()
 
 score = Score(canvas, 'green')
-paddle = Paddle(canvas, 'White')
-brick = Brick(canvas, 'orange')
-ball = Ball(canvas, paddle, score, brick, 'red')
+paddle = Paddle(canvas, 'yellow')
+brick = Brick(canvas, 'red')
+ball = Ball(canvas, paddle, score, brick, 'white')
 
 while not ball.hit_bottom:
     if paddle.started == True:
@@ -22,9 +22,10 @@ while not ball.hit_bottom:
     tk.update_idletasks()
     tk.update()
     time.sleep(0.01)
+time.sleep(1)
 
 
-lbl = Label(tk, text="Вы достигли дна", font=("Arial Bold", 50))
-tk.update_idletasks()
-tk.update()
-time.sleep(3)
+#lbl = Label(tk, text="Вы достигли дна", font=("Arial Bold", 50))
+#lbl.draw()
+#tk.update_idletasks()
+#tk.update()
