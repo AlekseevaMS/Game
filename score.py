604c17720c5737f230da3d59065bb40fdb7e2f7c
shmarcell <kosichkina.di@phystech.edu>
2019-12-19 15:39:56 +0300

    соре

--- score.py
+++ score.py
@@ -2,10 +2,10 @@ class Score:
 
     # Описание счета для игры(мало ли кому-то интересно)
 
-    def __init__(self, canvas, color):
+    def __init__(self, pos_x, canvas, color):
         self.score = 0#обнуляем
         self.canvas = canvas
-        self.id = canvas.create_text(450, 10, text=self.score, font=('Courier', 15), fill=color)
+        self.id = canvas.create_text(pos_x, 10, text=self.score, font=('Courier', 15), fill=color)
 
     #Касание платформы
 
