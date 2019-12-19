e0374edefa5ca6a798f36e718432d43b56ba7496
AlekseevaMS <Alekseeva.MS@phystech.edu>
2019-12-19 04:40:37 +0300

    подредаченно2

--- Brick.py
+++ Brick.py
@@ -14,16 +14,21 @@ class Brick:
 
         self.canvas = canvas
         self.id = canvas.create_rectangle(0, 0, 50, 10, fill=color)
+        #self.live = live
         start_1 = []# список возможных стартовых положений
         for i in range(50):
             start_1.append(random.randint(60, 340))
         random.shuffle(start_1)
         self.starting_point_x = start_1[0]#стартовое положение
-        self.canvas.move(self.id, self.starting_point_x, 100)
+        start_2 = []# список возможных стартовых положений
+        for i in range(50):
+            start_2.append(random.randint(60, 140))
+        random.shuffle(start_2)
+        self.starting_point_y = start_2[0]
+        self.canvas.move(self.id, self.starting_point_x, self.starting_point_y)
         self.x = 0
         self.canvas_width = self.canvas.winfo_width()
         self.canvas_width = self.canvas.winfo_width()
-        self.live = 1
 
 
 #    def draw(self):#движение кирпич
