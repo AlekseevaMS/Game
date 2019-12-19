eb616eeca081a89bb353ce995bc6e133e6cb4099
AlekseevaMS <Alekseeva.MS@phystech.edu>
2019-12-19 13:48:16 +0300

    Хех, надеюсь работает

---
+++ Hockey.py
@@ -0,0 +1,43 @@
+from tkinter import *
+from Puck import *
+from Paddle import *
+from score import *
+from Brick import *
+from rrr import *
+import time
+
+
+tk = Tk()
+tk.title('Game')
+canvas = Canvas(tk, width=500, height=400, highlightthickness=0, bg ='black')
+canvas.pack()
+tk.update()
+
+score = Score(canvas, 'turquoise')
+paddle = Paddle(canvas, 'yellow')
+paddle2 = Paddle2(canvas, 'yellow')
+#BRICKS = []
+#for i in range(10):
+#    BRICKS.append(Brick(canvas, 'red'))
+#for i in range(10):
+#    brick = BRICKS[i]
+brick = Brick(canvas, 'red')
+puck = Puck(canvas, paddle, paddle2, score, brick, 'white')
+
+
+
+while not (puck.hit_bottom and puck.hit_top):
+    if (paddle.started or paddle2.started) is True:
+        puck.draw()
+        paddle.draw()
+        paddle2.draw()
+    tk.update_idletasks()
+    tk.update()
+    time.sleep(0.01)
+time.sleep(1)
+
+
+#lbl = Label(tk, text="Вы достигли дна", font=("Arial Bold", 50))
+#lbl.draw()
+#tk.update_idletasks()
+#tk.update()
